e6d05d506cebec996e7aa2796e96ed4531aadca5
ayocesarallie37 <2202002@utrivieramaya.edu.mx>
2024-04-03 13:37:44 -0600

    update

--- login.php
+++ login.php
@@ -17,6 +17,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="login/style.css" />
   <title>Sign in & Sign up Form</title>
+  <script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore-compat.js"></script>
 </head>
 
 <body>
